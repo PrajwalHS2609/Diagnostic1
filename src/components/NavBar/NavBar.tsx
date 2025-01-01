740e51438191ce2b18653011d9659c0b0dbb1ecd
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-01 16:33:48 +0530

    navbar changes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,31 +1,10 @@
-"use client";
-import { useState, useEffect } from "react";
 import Logo from "./Logo";
 import Menu from "./Menu";
 import "./NavBar.css";
 
 export default function Navbar() {
-  const [scroll, setScroll] = useState(false);
-
-  useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 90) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-
-    window.addEventListener("scroll", changeColor);
-
-    // Cleanup the event listener on unmount
-    return () => {
-      window.removeEventListener("scroll", changeColor);
-    };
-  }, []); // Empty dependency array ensures this runs only once on mount
-
   return (
-    <nav className={scroll ? "navbar-container" : "navbar-container1"}>
+    <nav className="navbar-container">
       <Logo />
       <Menu />
     </nav>
